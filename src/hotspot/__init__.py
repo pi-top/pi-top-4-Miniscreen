9e72cd9cb6e612b053861290db0759f4f3bcc227
m-roberts <2947595+m-roberts@users.noreply.github.com>
2018-11-05 15:40:07 +0800

    Re-arrange hotspot widget file structure

--- src/hotspot/__init__.py
+++ src/hotspot/__init__.py
@@ -1,4 +1,6 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Copyright (c) 2014-18 Richard Hull and contributors
-# See LICENSE.rst for details.
+try:
+    from pkg_resources import declare_namespace
+    declare_namespace(__name__)
+except ImportError:
+    from pkgutil import extend_path
+    __path__ = extend_path(__path__, __name__)
