2f5d86d37cf5b5cd88f766abed33d31d004b50d3
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2019-01-01 00:13:24 +0000

    ✨ Add base hotspot/snapshot class
    This makes widgets capable of being sub-classed and therefore capable of being stateful.

---
+++ src/components/widgets/common/base_widget_hotspot.py
@@ -0,0 +1,49 @@
+from time import time
+from luma.core.virtual import (
+    hotspot,
+    snapshot
+)
+
+
+class BaseStaticHotspot(hotspot):
+    def __init__(self, width, height, render_func):
+        super(BaseStaticHotspot, self).__init__(width, height)
+        self.redraw = True
+
+        if callable(render_func):
+            self._render = render_func
+        else:
+            raise Exception("Render function is not callable")
+
+    def should_redraw(self):
+        # TODO: Find out why this is constantly going true again
+        # print("Redraw? " + str(self.redraw))
+        return self.redraw
+
+    def update(self, draw):
+        if self._render is not None and self.redraw is True:
+            self._render(draw, self.width, self.height)
+            self.redraw = False
+
+
+class BaseSnapshot(snapshot):
+    def __init__(self, width, height, interval, render_func):
+        super(BaseSnapshot, self).__init__(width, height, interval)
+        self._interval = interval
+        self._last_updated = 0
+        if callable(render_func):
+            self._render = render_func
+        else:
+            raise Exception("Render function is not callable")
+
+    def update_last_updated(self):
+        self._last_updated = time()
+
+    def should_redraw(self):
+        return time() - self._last_updated > self._interval
+
+    def update(self, draw):
+        if self._render is not None:
+            self._render(draw, self.width, self.height)
+
+        self.update_last_updated()
