97442ef8105cd7c16aff7b30e4e075d76ab3067b
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2019-01-01 00:28:15 +0000

    🎨 Use base hotspot/snapshot class for widgets 2
    Include template widgets

--- src/components/widgets/main/template.py
+++ src/components/widgets/main/template.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from components.widgets.common_functions import title_text
+from components.widgets.common.base_widget_hotspot import BaseStaticHotspot
 
 
 def page(title="Title"):
@@ -9,3 +10,8 @@ def page(title="Title"):
         title_text(draw, margin, width, text=title)
 
     return render
+
+
+class StaticHotspot(BaseStaticHotspot):
+    def __init__(self, width, height, render_func):
+        super(StaticHotspot, self).__init__(width, height, render_func)
--- src/components/widgets/projects/template.py
+++ src/components/widgets/projects/template.py
@@ -4,6 +4,7 @@
 # See LICENSE.rst for details.
 
 from PIL import Image
+from components.widgets.common.base_widget_hotspot import BaseStaticHotspot
 
 
 def project(title="My Project", img_path=""):
@@ -15,3 +16,8 @@ def project(title="My Project", img_path=""):
             img_bitmap = Image.open(img_path).convert("RGBA")
             draw.bitmap((width/4, height/6), img_bitmap.resize((int(width/2), int(height/2))), fill="white")
     return render
+
+
+class StaticHotspot(BaseStaticHotspot):
+    def __init__(self, width, height, render_func):
+        super(StaticHotspot, self).__init__(width, height, render_func)
