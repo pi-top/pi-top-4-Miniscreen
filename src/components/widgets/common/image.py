d231dc3b4bd8923eb639387c49acba03824a7195
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2019-01-02 17:18:45 +0000

    ✨ Improve base image widget
    Render to whole display. Add ability to stop animation looping. Animation interval of GIF handled by GIF metadata. Check for valid image file.

--- src/components/widgets/common/image.py
+++ src/components/widgets/common/image.py
@@ -1,11 +1,20 @@
 from PIL import Image
+from os.path import isfile
 from components.widgets.common.base_widget_hotspot import BaseHotspot
 
 
 def _create_bitmap_to_render(image, width, height):
     size_tuple = (width, height)
-    size = [min(*size_tuple)] * 2
-    position = ((width - size[0]) // 2, height - size[1])
+    # # Forces a square - [64, 64]
+    # size = [min(*size_tuple)] * 2
+    #
+    # # Creates offset - (32, 0)
+    # position = ((width - size[0]) // 2, height - size[1])
+    # print(position)
+
+    # Full screen
+    size = [width, height]
+    position = (0, 0)
 
     # Create new image with same size as display
     img_bitmap = Image.new("RGBA", size_tuple)
@@ -25,16 +34,17 @@ class Hotspot(BaseHotspot):
         self._error = True  # Until image is set
 
         self.image_path = None
+        self.loop = True
         self.update_props(**data)
-
-        if not self._image.is_animated and interval >= 0.0:
-            print(
-                "Warning - image is not animated, but has a redraw interval speed."
-                "This may affect performance")
 
     def _load_image_from_path(self, value):
         self._error = False
         self.image_path = value
+        if not isfile(self.image_path):
+            print("Invalid path for image file")
+            self._error = True
+            return
+
         try:
             self._image = Image.open(self.image_path)
             self._image.verify()
@@ -49,21 +59,27 @@ class Hotspot(BaseHotspot):
             print(e)
             self._error = True
 
-    def update_props(self, **data):
-        for key, value in data.items():
-            if key == "image_path":
-                self._load_image_from_path(value)
-                break
-
-    def render(self, draw, width, height):
-        if self._error is False and self._image is not None:
-            self._seek_next_frame_in_image()
-            draw.bitmap(xy=(0, 0), bitmap=_create_bitmap_to_render(self._image, width, height), fill="white")
-
     def _seek_next_frame_in_image(self):
         if self._image.is_animated:
             if self._frame_no + 1 < self._image.n_frames:
                 self._frame_no += 1
-            else:
+            elif self.loop:
                 self._frame_no = 0
-            self._image.seek(self._frame_no)
+
+        self._image.seek(self._frame_no)
+
+        if self._image.is_animated:
+            self.interval = float(self._image.info['duration'] / 1000)
+
+    def update_props(self, **data):
+        for key, value in data.items():
+            if key == "image_path":
+                self._load_image_from_path(value)
+            if key == "loop":
+                self.loop = value
+
+    def render(self, draw, width, height):
+        if self._error is False and self._image is not None:
+            self._seek_next_frame_in_image()
+            img_bitmap = _create_bitmap_to_render(self._image, width, height)
+            draw.bitmap(xy=(0, 0), bitmap=img_bitmap, fill="white")
