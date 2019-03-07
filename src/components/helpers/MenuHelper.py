7682702ea453498b6af65ac151d2e9c6dea22f50
ItsChoudhry <23042676+ItsChoudhry@users.noreply.github.com>
2019-03-07 14:56:43 +0000

    :art: Add date and time page and bug vnc info page bug

--- src/components/helpers/MenuHelper.py
+++ src/components/helpers/MenuHelper.py
@@ -11,6 +11,7 @@ from components.widgets.sys_info import (
     wifi,
     network,
     vnc_info,
+    date_time,
 )
 from components.widgets.main import template as main_menu_page
 from components.widgets.settings import template as settings_menu_page
@@ -197,6 +198,12 @@ class Pages:
             select_action_func=change_menu(Menus.MAIN_MENU),
             cancel_action_func=None,
         )
+        DATE_TIME = MenuPage(
+            name="date_time",
+            hotspot=get_hotspot(date_time, interval=1.0),
+            select_action_func=change_menu(Menus.MAIN_MENU),
+            cancel_action_func=None,
+        )
 
     class MainMenu(Enum):
         PROJECT_SELECT = MenuPage(
@@ -321,7 +328,7 @@ class Pages:
         VNC_SETUP = MenuPage(
             name="vnc",
             hotspot=get_hotspot(vnc_info, interval=1.0),
-            select_action_func=change_menu(Menus.MAIN_MENU),
+            select_action_func=None,
             cancel_action_func=None,
         )
 
