06970de3393beda9686b7807b88873547df906ef
ItsChoudhry <23042676+ItsChoudhry@users.noreply.github.com>
2019-03-10 15:17:12 +0000

    :sparkles: Add startup audio
    The 1.2 second sleep is to line up the audio and with the startup animaiton better.

--- src/components/widgets/startup_animation/startup_animation.py
+++ src/components/widgets/startup_animation/startup_animation.py
@@ -2,14 +2,38 @@ from os import path
 from time import sleep
 from PIL import Image, ImageSequence, ImageOps
 from components.System import device
+from pygame import mixer
 
 
 def play_animation():
-    img_path = path.abspath(path.join(path.dirname(__file__), "..", "..", "..", 'images', 'pi-top_startup.gif'))
+    img_path = path.abspath(
+        path.join(
+            path.dirname(__file__), "..", "..", "..", "images", "pi-top_startup.gif"
+        )
+    )
     img = Image.open(img_path)
+    play_startup_sound()
+    sleep(1.2)
     for frame in ImageSequence.Iterator(img):
         # inverted_frame = ImageOps.invert(frame.convert("RGB"))
         background = Image.new("RGB", device.size, "black")
         background.paste(frame.resize(device.size))
         device.display(background.convert(device.mode))
         sleep(0.1)
+
+
+def play_startup_sound():
+    mixer.init()
+    mixer.music.load(
+        path.abspath(
+            path.join(
+                path.dirname(__file__),
+                "..",
+                "..",
+                "..",
+                "audio",
+                "pi-top_startup_audio.mp3",
+            )
+        )
+    )
+    mixer.music.play()
