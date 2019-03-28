a7c94491161374e3d7d2688d25e1554ec09d982d
ItsChoudhry <23042676+ItsChoudhry@users.noreply.github.com>
2019-03-28 13:20:33 +0000

    Correct string comparison in get_image

--- src/components/widgets/settings/template.py
+++ src/components/widgets/settings/template.py
@@ -1,5 +1,4 @@
 from components.widgets.common_functions import get_file
-from components.widgets.common_values import default_margin_y
 from components.widgets.common.base_widget_hotspot import BaseHotspot
 from components.widgets.common.image_component import ImageComponent
 
@@ -13,8 +12,7 @@ class Hotspot(BaseHotspot):
 
     def get_image(self):
         state = self.method()
-
-        if state == "enabled":
+        if state == "Enabled":
             return get_file("settings_pages/" + self.type + "_on.gif")
         else:
             return get_file("settings_pages/" + self.type + "_off.gif")
