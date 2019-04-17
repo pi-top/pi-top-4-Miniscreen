cf9a690caf655d7907394c8ae4ca97214045e349
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2019-04-17 14:47:44 +0800

    Improve battery level page

--- src/components/widgets/sys_info/batt_level.py
+++ src/components/widgets/sys_info/batt_level.py
@@ -11,33 +11,48 @@ class Hotspot(BaseHotspot):
         self.gif = ImageComponent(
             image_path=get_image_file("battery_shell_empty.gif"), loop=False
         )
+        self.battery_percentage = ""
+        self.charging = False
+
+    def update_battery_state(self):
         self.battery_percentage = get_battery_capacity()
+        self.charging = get_battery_charging_state() == "charging"
+
 
     def draw_battery_percentage(self, draw, width, height):
-        # magic numbers are used because the images assets are same as the page
-        # so can't be used to get relative values
         try:
+            # Remove '%' for evaluating bar width
             percentage = int(self.battery_percentage[:-1])
         except ValueError:
             percentage = 0
+
+        # Magic numbers are used because the images assets are same as the page
+        # so can't be used to get relative values
         top_margin = 25
         bottom_margin = 38
         left_margin = 14
         bar_width = left_margin + ((50 - left_margin) * (percentage / 100))
+
         draw.rectangle(
             (left_margin, top_margin) + (bar_width, bottom_margin), "white", "white"
         )
 
     def render(self, draw, width, height):
+        self.update_battery_state()
+
+        self.draw_battery_percentage(draw, width, height)
+
+        if self.charging:
+            if self.gif.image_path != "/usr/share/pt-sys-oled/images/battery_shell_charging.gif":
+                self.gif = ImageComponent(image_path=get_image_file("battery_shell_charging.gif"), loop=False)
+                self.gif.render(draw)
+        else:
+            if self.gif.image_path != "/usr/share/pt-sys-oled/images/battery_shell_empty.gif":
+                self.gif = ImageComponent(image_path=get_image_file("battery_shell_empty.gif"), loop=False)
+                self.gif.render(draw)
+
         x_margin = 69
         y_margin = 21
-        if get_battery_charging_state() == "charging":
-            battery_state = get_image_file("battery_shell_charging.gif")
-        else:
-            battery_state = get_image_file("battery_shell_empty.gif")
-            self.draw_battery_percentage(draw, width, height)
-        self.gif = ImageComponent(image_path=battery_state, loop=False)
-        self.gif.render(draw)
         draw_text(
-            draw, xy=(x_margin, y_margin), text=get_battery_capacity(), font_size=18
+            draw, xy=(x_margin, y_margin), text=self.battery_percentage, font_size=18
         )
