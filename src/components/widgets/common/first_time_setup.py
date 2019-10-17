7e34533250da76fb11e647a43e3a4451450a080d
ItsChoudhry <23042676+ItsChoudhry@users.noreply.github.com>
2019-10-17 16:17:15 +0100

    Initial commit

--- src/components/widgets/common/first_time_setup.py
+++ src/components/widgets/common/first_time_setup.py
@@ -31,8 +31,10 @@ def get_connected_device_ip():
 class Hotspot(BaseHotspot):
     def __init__(self, width, height, interval, **data):
         super(Hotspot, self).__init__(width, height, interval, self.render)
-        self.gif = ImageComponent(
-            image_path=get_image_file("first_time_connect.gif"), loop=True, playback_speed=1.0)
+        self.vnc_gif = ImageComponent(
+            image_path=get_image_file("vnc_page.gif"), loop=False, playback_speed=2.0)
+        self.connect_gif = ImageComponent(
+            image_path=get_image_file("first_time_connect.gif"), loop=True, playback_speed=2.0)
 
         self.ptusb0_ip = ""
         self.connected_device_ip = ""
@@ -41,16 +43,11 @@ class Hotspot(BaseHotspot):
         self.username = "pi" if getuser() == "root" else getuser()
         self.password = "pi-top"
 
-        self.is_moving_to_show_data = True
-        self.finished = False
-
-        self.x = 0
-
         self.default_interval = self.interval
 
     def reset(self):
-        self.gif = ImageComponent(
-            image_path=get_image_file("first_time_connect.gif"), loop=True, playback_speed=1.0)
+        self.vnc_gif = ImageComponent(
+            image_path=get_image_file("vnc_page.gif"), loop=False, playback_speed=2.0)
 
         self.ptusb0_ip = ""
         self.connected_device_ip = ""
@@ -69,56 +66,56 @@ class Hotspot(BaseHotspot):
 
         self.connected_device_ip = get_connected_device_ip()
 
-        self.is_moving_to_show_data = self.is_connected() and not self.x <= -128
+        if not self.is_connected():
+            self.vnc_gif = ImageComponent(
+                image_path=get_image_file("vnc_page.gif"),
+                loop=False,
+                playback_speed=2.0,
+            )
         self.initialised = True
 
-    def gif_is_off_screen(self):
-        return self.x <= -128
-
     def render(self, draw, width, height):
-        # Determine connection state
-        if not self.is_moving_to_show_data:
+        first_frame = not self.initialised
+
+        # Determine initial connection state
+        if first_frame:
             self.set_data_members()
 
-        self.gif.hold_first_frame = self.is_connected()
+        # Determine connection state
+        if not self.vnc_gif.is_animating():
+            self.set_data_members()
 
-        if self.is_moving_to_show_data:
-            self.interval = 0.025
-
-            # Move GIF to the left
-            self.x -= 5
-
-            # Stop moving GIF to the left if off screen
-            if self.gif_is_off_screen:
-                self.is_moving_to_show_data = False
+        # Determine animation speed
+        # TODO: fix frame speed in GIF
+        # self.interval = self.vnc_gif.frame_duration
+        if first_frame:
+            self.interval = 0.5
         else:
-            if not self.is_connected():
-                # Reset GIF position
-                self.x = 0
-            self.interval = self.default_interval
+            if self.vnc_gif.is_animating():
+                self.interval = 0.025
+            else:
+                self.interval = self.default_interval
 
         # Draw to OLED
-        self.gif.xy = (self.x, 0)
-        self.gif.render(draw)
+        if not self.is_connected():
+            self.connect_gif.render(draw)
+        else:
+            self.vnc_gif.render(draw)
 
-        show_data = \
-            self.is_connected() and \
-            not self.is_moving_to_show_data and \
-            self.gif_is_off_screen()
-
-        if show_data:
-            draw_text(
-                draw,
-                xy=(default_margin_x, common_first_line_y),
-                text=str(self.username),
-            )
-            draw_text(
-                draw,
-                xy=(default_margin_x, common_second_line_y),
-                text=str(self.password),
-            )
-            draw_text(
-                draw,
-                xy=(default_margin_x, common_third_line_y),
-                text=str(self.ptusb0_ip)
-            )
+        if self.initialised and not self.vnc_gif.is_animating():
+            if self.is_connected() and self.vnc_gif.finished:
+                draw_text(
+                    draw,
+                    xy=(default_margin_x, common_first_line_y),
+                    text=str(self.username),
+                )
+                draw_text(
+                    draw,
+                    xy=(default_margin_x, common_second_line_y),
+                    text=str(self.password),
+                )
+                draw_text(
+                    draw,
+                    xy=(default_margin_x, common_third_line_y),
+                    text=str(self.ptusb0_ip)
+                )
