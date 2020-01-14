9b7a3395b513d9c154ba37aac09ff841149ef067
Fang <xcode6@163.com>
2020-01-14 11:18:12 +0800

    change shell script according to pre-commit logs

--- src/scripts/pt-start-access-point.sh
+++ src/scripts/pt-start-access-point.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 if [ -n "$1" ] && [ -n "$2" ] ;then
-sudo sed -i 's/^ssid=.*/ssid='$1'/' /home/pi/hostapd.conf
-sudo sed -i 's/^wpa_passphrase=.*/wpa_passphrase='$2'/' /home/pi/hostapd.conf
+	sed -i 's/^ssid=.*/ssid='"$1"'/' /home/pi/hostapd.conf
+	sed -i 's/^wpa_passphrase=.*/wpa_passphrase='"$2"'/' /home/pi/hostapd.conf
 fi
-sudo cp /home/pi/hostapd.conf  /etc/hostapd/hostapd.conf
-sudo sed -i 's/^#DAEMON_CONF=.*/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/'   /etc/default/hostapd
-sudo sed -i 's/^DAEMON_CONF=.*/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/'   /etc/default/hostapd
+cp /home/pi/hostapd.conf  /etc/hostapd/hostapd.conf
+sed -i 's/^#DAEMON_CONF=.*/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/'   /etc/default/hostapd
+sed -i 's/^DAEMON_CONF=.*/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/'   /etc/default/hostapd
 
-sudo sed -i 's/^#nohook wpa_supplicant/nohook wpa_supplicant/'  /etc/dhcpcd.conf
-sudo sed -i 's/^#static ip_address=192.168.63.1\/24/static ip_address=192.168.63.1\/24/'  /etc/dhcpcd.conf
-sudo sed -i 's/^#interface wlan0/interface wlan0/'  /etc/dhcpcd.conf
-sudo systemctl  restart dhcpcd
+sed -i 's/^#nohook wpa_supplicant/nohook wpa_supplicant/'  /etc/dhcpcd.conf
+sed -i 's/^#static ip_address=192.168.63.1\/24/static ip_address=192.168.63.1\/24/'  /etc/dhcpcd.conf
+sed -i 's/^#interface wlan0/interface wlan0/'  /etc/dhcpcd.conf
+systemctl  restart dhcpcd
 
-sudo systemctl unmask hostapd
-sudo systemctl start hostapd
-sudo systemctl enable hostapd
+systemctl unmask hostapd
+systemctl start hostapd
+systemctl enable hostapd
 
+sed -i 's/^INTERFACESv4=.*/INTERFACESv4="ptusb0 wlan0"/'  /etc/default/isc-dhcp-server
+/usr/bin/pt-dhcp-server  stop
+/usr/bin/pt-dhcp-server  start
 
-sudo sed -i 's/^INTERFACESv4=.*/INTERFACESv4="ptusb0 wlan0"/'  /etc/default/isc-dhcp-server
-sudo /usr/bin/pt-dhcp-server  stop
-sudo /usr/bin/pt-dhcp-server  start
+#ifconfig wlan0 192.168.63.1
 
-#sudo ifconfig wlan0 192.168.63.1
-
-sudo sed -i 's/^#net.ipv4.ip_forward=.*/net.ipv4.ip_forward=1/' /etc/sysctl.conf
-sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
-sudo iptables -t nat -A  POSTROUTING -o eth0 -j MASQUERADE
-sudo iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT 
-sudo iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
-sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
+sed -i 's/^#net.ipv4.ip_forward=.*/net.ipv4.ip_forward=1/' /etc/sysctl.conf
+sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
+iptables -t nat -A  POSTROUTING -o eth0 -j MASQUERADE
+iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
+sh -c "iptables-save > /etc/iptables.ipv4.nat"
--- src/scripts/pt-stop-access-point.sh
+++ src/scripts/pt-stop-access-point.sh
@@ -1,19 +1,19 @@
-sudo systemctl stop hostapd
-sudo systemctl disable hostapd
+#!/bin/bash
+systemctl stop hostapd
+systemctl disable hostapd
 
-sudo ifconfig wlan0 0.0.0.0
-sudo sed -i 's/^nohook wpa_supplicant/#nohook wpa_supplicant/'  /etc/dhcpcd.conf
-sudo sed -i 's/^static ip_address=192.168.63.1\/24/#static ip_address=192.168.63.1\/24/'  /etc/dhcpcd.conf
-sudo sed -i 's/^interface wlan0/#interface wlan0/'  /etc/dhcpcd.conf
-sudo systemctl restart dhcpcd
+ifconfig wlan0 0.0.0.0
+sed -i 's/^nohook wpa_supplicant/#nohook wpa_supplicant/'  /etc/dhcpcd.conf
+sed -i 's/^static ip_address=192.168.63.1\/24/#static ip_address=192.168.63.1\/24/'  /etc/dhcpcd.conf
+sed -i 's/^interface wlan0/#interface wlan0/'  /etc/dhcpcd.conf
+systemctl restart dhcpcd
 
-sudo sed -i 's/^INTERFACESv4=.*/INTERFACESv4="ptusb0"/'  /etc/default/isc-dhcp-server
-sudo /usr/bin/pt-dhcp-server  stop
-sudo /usr/bin/pt-dhcp-server  start
+sed -i 's/^INTERFACESv4=.*/INTERFACESv4="ptusb0"/'  /etc/default/isc-dhcp-server
+/usr/bin/pt-dhcp-server  stop
+/usr/bin/pt-dhcp-server  start
 
-sudo sed -i 's/^net.ipv4.ip_forward=.*/#net.ipv4.ip_forward=0/' /etc/sysctl.conf
-sudo sh -c "echo 0 > /proc/sys/net/ipv4/ip_forward"
-sudo iptables -D FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-sudo iptables -D FORWARD -i wlan0 -o eth0 -j ACCEPT
-sudo iptables -t nat -D  POSTROUTING -o eth0 -j MASQUERADE
-
+sed -i 's/^net.ipv4.ip_forward=.*/#net.ipv4.ip_forward=0/' /etc/sysctl.conf
+sh -c "echo 0 > /proc/sys/net/ipv4/ip_forward"
+iptables -D FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -D FORWARD -i wlan0 -o eth0 -j ACCEPT
+iptables -t nat -D  POSTROUTING -o eth0 -j MASQUERADE
