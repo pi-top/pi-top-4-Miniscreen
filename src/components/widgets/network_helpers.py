14a35b1b97581be5553abcef7024e06f4ae5285d
Jorge Capona <jcapona@gmail.com>
2020-03-31 10:32:34 -0300

    Use ping to check leased ip connection status

--- src/components/widgets/network_helpers.py
+++ src/components/widgets/network_helpers.py
@@ -1,4 +1,6 @@
 from isc_dhcp_leases import IscDhcpLeases
+from ptcommon.logger import PTLogger
+from ptcommon.command_runner import run_command
 
 
 def interface_is_up(interface_name):
@@ -10,10 +12,20 @@ def interface_is_up(interface_name):
 
 
 def get_address_for_ptusb_connected_device():
-    if interface_is_up("ptusb0"):
-        current_leases_dict = IscDhcpLeases(
-            '/var/lib/dhcp/dhcpd.leases').get_current()
-        for lease in current_leases_dict.values():
-            return lease.ip
+    if not interface_is_up("ptusb0"):
+        return ""
 
+    current_leases_dict = IscDhcpLeases(
+        '/var/lib/dhcp/dhcpd.leases').get_current()
+    for lease in current_leases_dict.values():
+        try:
+            PTLogger.info(
+                "Checking if leased ip {} is connected.".format(lease.ip))
+            run_command("ping -c1 {}".format(lease.ip),
+                        timeout=0.1, check=True)
+            PTLogger.info("IP {} is connected.".format(lease.ip))
+            return lease.ip
+        except Exception as e:
+            PTLogger.info("{} is not connected.".format(lease.ip))
+            continue
     return ""
