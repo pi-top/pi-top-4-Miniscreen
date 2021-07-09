3f590cb0e7ed0ac9b4429a2e8d61e0b73b36ea01
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2021-07-09 14:18:26 +0100

    Rework AP sys info page to use 2 static images for animation

--- src/components/widgets/sys_info/ap.py
+++ src/components/widgets/sys_info/ap.py
@@ -9,39 +9,84 @@ from components.widgets.common.values import (
 from components.widgets.common.base_widgets import BaseSnapshot
 from components.widgets.common.image_component import ImageComponent
 
+from enum import Enum
+from PIL import Image
+
+
+class RenderState(Enum):
+    STATIONARY = 0
+    ANIMATING = 1
+    DISPLAYING_INFO = 2
+
 
 class Hotspot(BaseSnapshot):
     def __init__(self, width, height, mode, interval, **data):
         super(Hotspot, self).__init__(width, height, interval, self.render)
         self.width = width
         self.height = height
+        self.size = (self.width, self.height)
         self.mode = mode
-        self.gif = ImageComponent(
-            device_mode=self.mode,
-            width=self.width,
-            height=self.height,
-            image_path=get_image_file_path("sys_info/ap.gif"),
-            loop=False,
-            playback_speed=2.0,
+
+        self.title_image_pos = (0, 0)
+
+        self.title_connected_image = process_image(
+            Image.open(get_image_file_path("sys_info/ap_title.png"))
         )
+
+        def add_disconnected_icon(pil_image):
+            pil_image_disconnected_draw = ImageDraw.Draw(pil_image)
+            pil_image_disconnected_draw.ellipse((70, 23) + (84, 37), 0, 0)
+            pil_image_disconnected_draw.ellipse((71, 24) + (83, 36), 1, 0)
+            pil_image_disconnected_draw.line((74, 27) + (79, 32), "black", 2)
+            pil_image_disconnected_draw.line((75, 32) + (80, 27), "black", 2)
+
+        self.title_disconnected_image = self.title_connected_image.copy()
+        add_disconnected_icon(self.title_disconnected_image)
+
+        self.info_image = process_image(
+            Image.open(get_image_file_path("sys_info/ap_info.png"))
+        )
+
+        self.default_interval = interval
 
         self.ssid = ""
         self.wlan0_ip = ""
         self.passphrase = ""
         self.initialised = False
 
-        self.default_interval = interval
+        self.interval = self.default_interval
+
+        self.render_state = RenderState.STATIONARY
+
+    def process_image(self, image_to_process):
+        if image_to_process.size == self.size:
+            image = image_to_process
+            if image.mode != self.mode:
+                image = image.convert(self.mode)
+        else:
+            image = Image.new(
+                self.mode,
+                self.size,
+                "black"
+            )
+            image.paste(
+                image_to_process.resize(
+                    self.size,
+                    resample=Image.NEAREST
+                )
+            )
+
+        return image
 
     def reset(self):
-        self.gif = ImageComponent(
-            device_mode=self.mode,
-            width=self.width,
-            height=self.height,
-            image_path=get_image_file_path("sys_info/ap.gif"),
-            loop=False,
-            playback_speed=2.0,
-        )
+        self.reset_animation()
+        self.reset_data_members()
 
+    def reset_animation(self):
+        self.title_image_pos = (0, 0)
+        self.render_state = RenderState.STATIONARY
+
+    def reset_data_members(self):
         self.ssid = ""
         self.wlan0_ip = ""
         self.passphrase = ""
@@ -59,62 +104,61 @@ class Hotspot(BaseSnapshot):
         self.passphrase = ap_data.get("passphrase", "")
 
         if not self.is_connected():
-            self.gif = ImageComponent(
-                device_mode=self.mode,
-                width=self.width,
-                height=self.height,
-                image_path=get_image_file_path("sys_info/ap.gif"),
-                loop=False,
-                playback_speed=2.0,
-            )
+            self.reset_animation()
 
-        self.gif.hold_first_frame = not self.is_connected()
         self.initialised = True
 
-    def render(self, draw, width, height):
-        first_frame = not self.initialised
-
-        # Determine initial connection state
-        if first_frame:
-            self.set_data_members()
-
-        # Determine connection state
-        if not self.gif.is_animating():
-            self.set_data_members()
-
-        # Determine animation speed
-        # TODO: fix frame speed in GIF
-        # self.interval = self.gif.frame_duration
-        if first_frame:
+    def set_interval(self):
+        if self.render_state == RenderState.STATIONARY:
             self.interval = 0.5
         else:
-            if self.gif.is_animating():
-                self.interval = 0.025
+            if self.render_state == RenderState.ANIMATING
+            self.interval = 0.025
             else:
                 self.interval = self.default_interval
 
-        # Draw to OLED
-        self.gif.render(draw)
+    def update_render_state(self):
+        if self.is_connected():
+            if self.title_image_pos[0] <= -self.width:
+                self.render_state = RenderState.DISPLAYING_INFO
 
-        if self.initialised and not self.gif.is_animating():
-            if self.is_connected() and self.gif.finished:
-                draw_text(
-                    draw,
-                    xy=(default_margin_x, common_first_line_y),
-                    text=str(self.ssid),
-                )
-                draw_text(
-                    draw,
-                    xy=(default_margin_x, common_second_line_y),
-                    text=str(self.passphrase),
-                )
-                draw_text(
-                    draw,
-                    xy=(default_margin_x, common_third_line_y),
-                    text=str(self.wlan0_ip)
-                )
-            elif not self.is_connected() and self.gif.hold_first_frame:
-                draw.ellipse((70, 23) + (84, 37), 0, 0)
-                draw.ellipse((71, 24) + (83, 36), 1, 0)
-                draw.line((74, 27) + (79, 32), "black", 2)
-                draw.line((75, 32) + (80, 27), "black", 2)
+            elif self.render_state != RenderState.DISPLAYING_INFO:
+                self.render_state = RenderState.ANIMATING
+                self.title_image_pos = (self.title_image_pos[0] - 1, 0)
+        else:
+            self.reset_animation()
+
+    def render(self, draw, width, height):
+        # Determine connection state
+        if not self.initialised or self.render_state == RenderState.DISPLAYING_INFO:
+            self.set_data_members()
+
+        self.update_render_state()
+
+        if self.render_state == RenderState.DISPLAYING_INFO:
+            draw_text(
+                draw,
+                xy=(default_margin_x, common_first_line_y),
+                text=str(self.ssid),
+            )
+            draw_text(
+                draw,
+                xy=(default_margin_x, common_second_line_y),
+                text=str(self.passphrase),
+            )
+            draw_text(
+                draw,
+                xy=(default_margin_x, common_third_line_y),
+                text=str(self.wlan0_ip)
+            )
+        else:
+            title_image = self.title_connected_image if self.is_connected() \
+                else self.title_disconnected_image
+
+            draw.bitmap(
+                xy=self.title_image_pos,
+                bitmap=title_image,
+                fill="white",
+            )
+
+        self.set_interval()
