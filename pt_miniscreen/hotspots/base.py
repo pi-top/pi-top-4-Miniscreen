e881ebe73d12001afbb031151030c9dbb8df8bde
m-roberts <m-roberts@users.noreply.github.com>
2021-10-26 03:57:23 +0100

    Use mask based on overlay property

--- pt_miniscreen/hotspots/base.py
+++ pt_miniscreen/hotspots/base.py
@@ -11,6 +11,8 @@ class HotspotBase:
         self.size = size
         self.mode = mode
 
+        self.overlay = False
+
         self.last_updated = -self.interval
         self.invert = False
         self.width = size[0]
@@ -26,12 +28,15 @@ class HotspotBase:
     def paste_into(self, image, xy):
         hotspot_image = Image.new(image.mode, self.size)
         self.render(hotspot_image)
+
         if self.invert:
             hotspot_image = MiniscreenAssistant(self.mode, self.size).invert(
                 hotspot_image
             )
-        mask = hotspot_image
+
+        mask = hotspot_image if self.overlay else None
         image.paste(hotspot_image, xy, mask)
+
         del hotspot_image
         self.last_updated = perf_counter()
 
