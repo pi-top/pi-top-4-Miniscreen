cee5b576cff22f68d014875398a4ad50343018df
m-roberts <m-roberts@users.noreply.github.com>
2021-10-26 14:21:59 +0100

    Fix app scroll check

--- pt_miniscreen/hotspots/base.py
+++ pt_miniscreen/hotspots/base.py
@@ -1,6 +1,6 @@
 from time import perf_counter
 
-from PIL import Image
+from PIL import Image, ImageOps
 from pitop.miniscreen.oled.assistant import MiniscreenAssistant
 
 
@@ -11,21 +11,25 @@ class HotspotBase:
         self.size = size
         self.mode = mode
 
-        self.overlay = True
+        self.draw_white = True
+        self.draw_black = False
 
         self.last_updated = -self.interval
         self.invert = False
         self.width = size[0]
         self.height = size[1]
-        # self.visible = True
-        # self.font_size = 14
-        # self.wrap = True
 
     def should_redraw(self):
+        if not self.draw_white and not self.draw_black:
+            return False
+
         """Only requests a redraw after ``interval`` seconds have elapsed."""
         return perf_counter() - self.last_updated > self.interval
 
     def paste_into(self, image, xy):
+        if not self.draw_white and not self.draw_black:
+            return
+
         hotspot_image = Image.new(image.mode, self.size)
         self.render(hotspot_image)
 
@@ -34,7 +38,13 @@ class HotspotBase:
                 hotspot_image
             )
 
-        mask = hotspot_image if self.overlay else None
+        mask = None
+        if self.draw_white and not self.draw_black:
+            mask = hotspot_image
+
+        elif not self.draw_white and self.draw_black:
+            mask = ImageOps.invert(hotspot_image)
+
         image.paste(hotspot_image, xy, mask)
 
         del hotspot_image
