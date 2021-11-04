036d02624200f7068003b4dc09194bb5c333cebb
Jorge Capona <jcapona@gmail.com>
2021-11-04 17:20:20 -0400

    Consider title bar interval to redraw

--- pt_miniscreen/app.py
+++ pt_miniscreen/app.py
@@ -203,7 +203,10 @@ class App:
                     else:
                         interval = Speeds.SCROLL.value
                 else:
-                    interval = self.menu_manager.current_menu_page.interval
+                    interval = min(
+                        self.menu_manager.current_menu_page.interval,
+                        self.menu_manager.title_bar.interval,
+                    )
 
             logger.debug("Waiting until timeout or until page has changed...")
             self.menu_manager.wait_until_timeout_or_should_redraw_event(interval)
