c138b42496739335d21fca5de6cde6796123d549
Jorge Capona <jcapona@gmail.com>
2021-11-11 13:04:04 -0500

    Add enabled/disabled status images

--- pt_miniscreen/hotspots/status_icon_hotspot.py
+++ pt_miniscreen/hotspots/status_icon_hotspot.py
@@ -9,11 +9,118 @@ from .base import Hotspot as HotspotBase
 logger = logging.getLogger(__name__)
 
 
+def tick_image(size, mode, scale, line_width):
+    image = PIL.Image.new(mode, size)
+    PIL.ImageDraw.Draw(image).ellipse(
+        xy=[(0, 0), (image.width - 1, image.height - 1)],
+        fill="white",
+    )
+    center = (image.width / 2, image.height / 2)
+    internal_square_size = (scale * image.width, scale * image.height)
+    internal_square_top_left = (
+        image.width * (1 - scale) / 2 - 1,
+        image.height * (1 - scale) / 2 - 1,
+    )
+
+    tick_top_right_x = internal_square_top_left[0] + internal_square_size[0] + 1
+    tick_top_left_y = internal_square_top_left[1] + internal_square_size[1] * 5 / 8
+    joint_xy = (
+        center[0] - internal_square_size[0] / 4,
+        center[1] + internal_square_size[1] / 2,
+    )
+
+    for xy in [
+        joint_xy + (tick_top_right_x, internal_square_top_left[1]),
+        joint_xy + (internal_square_top_left[0], tick_top_left_y),
+    ]:
+        PIL.ImageDraw.Draw(image).line(
+            xy=xy,
+            width=line_width,
+            fill="black",
+            joint="curve",
+        )
+    PIL.ImageDraw.Draw(image).ellipse(
+        xy=[
+            (joint_xy[0] - line_width / 2, joint_xy[1] - line_width / 2),
+            (joint_xy[0] + line_width / 2, joint_xy[1] + line_width / 2),
+        ],
+        fill="black",
+    )
+    return image
+
+
+def cross_image(size, mode, scale, line_width):
+    image = PIL.Image.new(mode, size)
+    PIL.ImageDraw.Draw(image).ellipse(
+        xy=[(0, 0), (image.width - 1, image.height - 1)],
+        fill="white",
+    )
+    internal_square_size = (scale * image.width, scale * image.height)
+    internal_square_top_left = (
+        int(image.width * (1 - scale) / 2 - 1),
+        int(image.height * (1 - scale) / 2 - 1),
+    )
+    internal_square_bottom_right = (
+        internal_square_top_left[0] + internal_square_size[0] + 1,
+        internal_square_top_left[1] + internal_square_size[1] + 1,
+    )
+
+    for xy in [
+        internal_square_top_left + internal_square_bottom_right,
+        (internal_square_top_left[0], internal_square_bottom_right[1])
+        + (internal_square_bottom_right[0], internal_square_top_left[1]),
+    ]:
+        PIL.ImageDraw.Draw(image).line(xy=xy, width=line_width, fill="black")
+    return image
+
+
+def processing_image(size, mode, frame_number):
+    image = PIL.Image.new(mode, size)
+    PIL.ImageDraw.Draw(image).ellipse(
+        xy=[(0, 0), (image.width - 1, image.height - 1)],
+        fill="white",
+    )
+    circle_size = max(2, floor(image.width / 8))
+    center = (image.width / 2, image.height / 2)
+    off_left = (center[0] / 2, center[1])
+    off_right = (center[0] * 3 / 2, center[1])
+
+    for index, dot in enumerate([off_left, center, off_right]):
+        if index > frame_number:
+            break
+
+        PIL.ImageDraw.Draw(image).ellipse(
+            xy=[
+                (
+                    int(dot[0] - floor(circle_size / 2)),
+                    int(dot[1] - floor(circle_size / 2)),
+                ),
+                (
+                    int(dot[0] + floor(circle_size / 2)),
+                    int(dot[1] + floor(circle_size / 2)),
+                ),
+            ],
+            fill="black",
+        )
+    return image
+
+
 class Hotspot(HotspotBase):
     def __init__(self, interval, size, mode):
         super().__init__(interval, size, mode)
         self.processing_frame_no = 0
-        self.circle_size = max(2, floor(size[0] / 8))
+
+        self.MAX_PROCESSING_STEPS = 3
+        self.status_images = {
+            ActionState.UNKNOWN: cross_image(size, mode, scale=0.5, line_width=1),
+            ActionState.ENABLED: tick_image(size, mode, scale=0.5, line_width=3),
+            ActionState.DISABLED: cross_image(size, mode, scale=0.5, line_width=3),
+            ActionState.PROCESSING: {
+                frame_number: processing_image(size, mode, frame_number=frame_number)
+                for frame_number in range(self.MAX_PROCESSING_STEPS)
+            },
+        }
+
         self._action_state = ActionState.UNKNOWN
 
     @property
@@ -27,45 +134,16 @@ class Hotspot(HotspotBase):
 
         self._action_state = state
 
+    @property
+    def status_image(self):
+        if self.action_state == ActionState.PROCESSING:
+            self.processing_frame_no = (
+                self.processing_frame_no + 1
+            ) % self.MAX_PROCESSING_STEPS
+            return self.status_images.get(self.action_state).get(
+                self.processing_frame_no
+            )
+        return self.status_images.get(self.action_state)
+
     def render(self, image):
-        PIL.ImageDraw.Draw(image).ellipse(
-            xy=[(0, 0), self.size],
-            fill="white",
-        )
-
-        if self.action_state == ActionState.UNKNOWN:
-            # TODO: question mark
-            pass
-
-        elif self.action_state == ActionState.ENABLED:
-            # TODO: check mark
-            pass
-
-        elif self.action_state == ActionState.DISABLED:
-            # TODO: cross
-            pass
-
-        elif self.action_state == ActionState.PROCESSING:
-            center = (self.size[0] / 2, self.size[1] / 2)
-            off_left = (center[0] / 2, center[1])
-            off_right = (center[0] * 3 / 2, center[1])
-
-            for index, dot in enumerate([off_left, center, off_right]):
-                if index > self.processing_frame_no:
-                    break
-
-                PIL.ImageDraw.Draw(image).ellipse(
-                    xy=[
-                        (
-                            int(dot[0] - floor(self.circle_size / 2)),
-                            int(dot[1] - floor(self.circle_size / 2)),
-                        ),
-                        (
-                            int(dot[0] + floor(self.circle_size / 2)),
-                            int(dot[1] + floor(self.circle_size / 2)),
-                        ),
-                    ],
-                    fill="black",
-                )
-
-            self.processing_frame_no = (self.processing_frame_no + 1) % 3
+        image.paste(self.status_image)
