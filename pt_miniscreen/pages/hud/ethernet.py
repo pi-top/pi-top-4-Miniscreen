ad7621ddae9400c438c540713742467861a074c3
Jorge Capona <jcapona@gmail.com>
2021-11-19 13:37:42 -0500

    Fix ethernet page

--- pt_miniscreen/pages/hud/ethernet.py
+++ pt_miniscreen/pages/hud/ethernet.py
@@ -19,9 +19,9 @@ class Page(PageBase):
         MARGIN_X_LEFT = 30
         MARGIN_X_RIGHT = 10
         SCALE = self.height / 64.0
-        ICON_HEIGHT = 12
+        ICON_SIZE = 12
         VERTICAL_SPACING = 4
-        ROW_HEIGHT = ICON_HEIGHT + VERTICAL_SPACING
+        ROW_HEIGHT = ICON_SIZE + VERTICAL_SPACING
         DELTA_Y = int(ROW_HEIGHT * SCALE)
         COMMON_FIRST_LINE_Y = int(10 * SCALE)
         COMMON_SECOND_LINE_Y = COMMON_FIRST_LINE_Y + DELTA_Y
@@ -29,13 +29,12 @@ class Page(PageBase):
         DEFAULT_FONT_SIZE = 12
 
         self.hotspots: Dict = {
-            (0, 0): [
+            (ICON_X_POS, COMMON_SECOND_LINE_Y): [
                 ImageHotspot(
                     interval=self.interval,
                     mode=self.mode,
-                    size=self.size,
+                    size=(ICON_SIZE, ICON_SIZE),
                     image_path=get_image_file_path("sys_info/networking/home.png"),
-                    xy=(ICON_X_POS, COMMON_SECOND_LINE_Y),
                 ),
             ],
             (MARGIN_X_LEFT, COMMON_SECOND_LINE_Y - 1): [
@@ -43,7 +42,7 @@ class Page(PageBase):
                     interval=Speeds.MARQUEE.value,
                     mode=self.mode,
                     size=(self.width - MARGIN_X_LEFT - MARGIN_X_RIGHT, ROW_HEIGHT),
-                    text=lambda: get_internal_ip(iface="wlan0"),
+                    text=lambda: get_internal_ip(iface="eth0"),
                     font_size=DEFAULT_FONT_SIZE,
                 ),
             ],
