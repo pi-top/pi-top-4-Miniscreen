8152cdda1cb16f7f50326e1940ae556e47c392fe
Jorge Capona <jcapona@gmail.com>
2022-04-21 15:16:28 -0400

    ArrowDrawing -> utils.arrow

--- pt_miniscreen/core/components/arrow_navigation_indicator.py
+++ pt_miniscreen/core/components/arrow_navigation_indicator.py
@@ -1,42 +1,10 @@
 import logging
-from enum import Enum, auto
-
-from PIL import ImageDraw
+from functools import partial
 
 from pt_miniscreen.core.component import Component
-from pt_miniscreen.core.utils import apply_layers, layer
+from pt_miniscreen.core.utils import apply_layers, arrow, layer
 
 logger = logging.getLogger(__name__)
-
-
-class ArrowPosition(Enum):
-    TOP = auto()
-    BOTTOM = auto()
-
-
-class ArrowDrawing(Component):
-    def __init__(self, position, visible, **kwargs):
-        super().__init__(
-            **kwargs,
-            initial_state={
-                "position": position,
-                "visible": visible,
-            },
-        )
-
-    def render(self, image):
-        if self.state.get("position") == ArrowPosition.TOP and self.state["visible"]:
-            ImageDraw.Draw(image).regular_polygon(((3, 3), 4), 3, fill=1)
-        elif (
-            self.state.get("position") == ArrowPosition.BOTTOM and self.state["visible"]
-        ):
-            ImageDraw.Draw(image).regular_polygon(
-                ((3, image.size[1] - 4), 4),
-                3,
-                fill=1,
-                rotation=180,
-            )
-        return image
 
 
 class ArrowNavigationIndicator(Component):
@@ -44,7 +12,7 @@ class ArrowNavigationIndicator(Component):
         self,
         upper_arrow_padding=(0, 0),
         lower_arrow_padding=(0, 0),
-        top_arrow_visible=True,
+        upper_arrow_visible=True,
         bottom_arrow_visible=True,
         **kwargs
     ):
@@ -53,35 +21,46 @@ class ArrowNavigationIndicator(Component):
             initial_state={
                 "upper_arrow_padding": upper_arrow_padding,
                 "lower_arrow_padding": lower_arrow_padding,
+                "upper_arrow_visible": upper_arrow_visible,
+                "bottom_arrow_visible": bottom_arrow_visible,
             },
         )
 
-        self.upper_arrow = self.create_child(
-            ArrowDrawing, position=ArrowPosition.TOP, visible=top_arrow_visible
-        )
-        self.lower_arrow = self.create_child(
-            ArrowDrawing,
-            position=ArrowPosition.BOTTOM,
-            visible=bottom_arrow_visible,
-        )
+    @property
+    def upper_arrow_visible(self):
+        return self.state["upper_arrow_visible"]
+
+    @upper_arrow_visible.setter
+    def upper_arrow_visible(self, value):
+        self.state.update({"upper_arrow_visible": value})
+
+    @property
+    def bottom_arrow_visible(self):
+        return self.state["bottom_arrow_visible"]
+
+    @bottom_arrow_visible.setter
+    def bottom_arrow_visible(self, value):
+        self.state.update({"bottom_arrow_visible": value})
 
     def render(self, image):
         layers = []
         arrow_size = (10, 10)
 
-        if self.upper_arrow.state["visible"]:
+        if self.upper_arrow_visible:
             layers.append(
                 layer(
-                    self.upper_arrow.render,
+                    arrow,
                     size=arrow_size,
                     pos=self.state["upper_arrow_padding"],
                 )
             )
 
-        if self.lower_arrow.state["visible"]:
+        if self.bottom_arrow_visible:
             icon_padding = self.state["lower_arrow_padding"]
             icon_pos = (icon_padding[0], image.height - arrow_size[1] - icon_padding[1])
 
-            layers.append(layer(self.lower_arrow.render, size=arrow_size, pos=icon_pos))
+            layers.append(
+                layer(partial(arrow, rotation=180), size=arrow_size, pos=icon_pos)
+            )
 
         return apply_layers(image, layers)
