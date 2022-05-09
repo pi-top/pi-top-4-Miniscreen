775de81150c76b49820d3679e668505200c6a414
Jorge Capona <jcapona@users.noreply.github.com>
2022-05-09 11:18:19 -0400

    Print/store displayed images for debugging (#256)

--- pt_miniscreen/core/app.py
+++ pt_miniscreen/core/app.py
@@ -1,4 +1,7 @@
+import datetime
 import logging
+from os import environ
+from pathlib import Path
 
 from PIL import Image
 
@@ -11,6 +14,14 @@ class App:
         assert Root is not None
         self.miniscreen = miniscreen
         self.Root = Root
+
+        self.saved_cache_frame_no = 0
+        self.timestamp = (
+            str(datetime.datetime.now())
+            .split(".")[0]
+            .replace(" ", "_")
+            .replace(":", "-")
+        )
 
     def start(self):
         self.root = self.Root(on_rerender=self.display)
@@ -26,7 +37,20 @@ class App:
     # frequently, which is expected. It's worth noting the import is not cached
     # since the module was not found so the search happens every import attempt
     def display(self):
+        image = self.root.render(Image.new("1", self.miniscreen.size))
+
+        # debug: print displayed image in terminal
+        if environ.get("IMGCAT", "0") == "1":
+            from imgcat import imgcat
+
+            imgcat(image)
+
+        # debug: store images in /tmp/pt-miniscreen
+        if environ.get("SAVE_CACHE", "0") == "1":
+            path = Path(f"/tmp/pt-miniscreen/{self.timestamp}")
+            path.mkdir(parents=True, exist_ok=True)
+            image.save(path / f"{str(self.saved_cache_frame_no).zfill(4)}.png")
+            self.saved_cache_frame_no += 1
+
         logger.debug("Update display")
-        self.miniscreen.device.display(
-            self.root.render(Image.new("1", self.miniscreen.size))
-        )
+        self.miniscreen.device.display(image)
