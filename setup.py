6ea63e58851fd61ccde18bc03554b3a574c01e89
Jorge Capona <jcapona@users.noreply.github.com>
2022-08-18 15:51:47 -0400

    Set package version using environment variable (#279)

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from os import environ
+
 import setuptools
 
 if __name__ == "__main__":
-    setuptools.setup(scripts=["scripts/pt-miniscreen-should-start"])
+    setuptools.setup(
+        scripts=["scripts/pt-miniscreen-should-start"],
+        version=environ.get("PYTHON_PACKAGE_VERSION", "0.0.1.dev1").replace('"', ""),
+    )
