643e45fcb52d037618eeb4fa5b3d83a8db94beed
Jorge Capona <jcapona@gmail.com>
2022-09-23 18:53:01 -0300

    Add CloudfareDnsPage to settings menu

---
+++ pt_miniscreen/pages/settings/cloudfare_dns.py
@@ -0,0 +1,12 @@
+from pt_miniscreen.actions import cloudfare_dns_is_set, toggle_cloudfare_dns
+from pt_miniscreen.components.action_page import ActionPage
+
+
+class CloudfareDnsPage(ActionPage):
+    def __init__(self, **kwargs):
+        super().__init__(
+            text="Cloudfare DNS",
+            action=toggle_cloudfare_dns,
+            get_enabled_state=cloudfare_dns_is_set,
+            **kwargs,
+        )
