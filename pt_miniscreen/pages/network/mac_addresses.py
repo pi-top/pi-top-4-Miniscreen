440fe74a7ea30eda6a9fca1e0f321e91fea9d9a6
Jorge Capona <jcapona@gmail.com>
2024-06-27 15:56:02 -0400

    Add test

--- pt_miniscreen/pages/network/mac_addresses.py
+++ pt_miniscreen/pages/network/mac_addresses.py
@@ -1,6 +1,6 @@
 from functools import partial
 
-import psutil
+from psutil import net_if_addrs, AF_LINK
 
 from pt_miniscreen.components.info_page import InfoPage
 from pt_miniscreen.core.components.marquee_text import MarqueeText
@@ -17,16 +17,15 @@ def iface_mac_address(iface_name: str) -> str:
     txt = ""
 
     try:
-        nics = psutil.net_if_addrs()
+        nics = net_if_addrs()
     except Exception:
         return txt
-
     if iface_name in nics:
         for data in nics[iface_name]:
             if (
                 hasattr(data, "family")
                 and hasattr(data, "address")
-                and data.family == psutil.AF_LINK
+                and data.family == AF_LINK
             ):
                 txt = f"{iface_name_lookup.get(iface_name)}: {data.address}"
                 break
@@ -42,8 +41,20 @@ class MacAddressesPage(InfoPage):
             **kwargs,
             title="MAC addresses",
             Rows=[
-                partial(Row, text=iface_mac_address("wlan0")),
-                partial(Row, text=iface_mac_address("eth0")),
-                partial(Row, text=iface_mac_address("wlan_ap0")),
+                partial(
+                    Row,
+                    text=iface_mac_address("wlan0"),
+                    get_text=lambda: iface_mac_address("wlan0"),
+                ),
+                partial(
+                    Row,
+                    text=iface_mac_address("eth0"),
+                    get_text=lambda: iface_mac_address("eth0"),
+                ),
+                partial(
+                    Row,
+                    text=iface_mac_address("wlan_ap0"),
+                    get_text=lambda: iface_mac_address("wlan_ap0"),
+                ),
             ],
         )
--- tests/network_test.py
+++ tests/network_test.py
@@ -1,4 +1,5 @@
 from time import sleep
+from unittest.mock import MagicMock
 
 import pytest
 
@@ -85,3 +86,45 @@ def test_usb(miniscreen, snapshot, internal_ip):
     internal_ip(page="usb", interface="ptusb0", ip="192.168.0.1")
     sleep(2)
     snapshot.assert_match(miniscreen.device.display_image, "connected.png")
+
+
+def test_mac(miniscreen, snapshot, mocker):
+    AF_LINK = 1
+
+    def mock_net_if_addrs(with_ap):
+        mocker.patch("pt_miniscreen.pages.network.mac_addresses.AF_LINK", AF_LINK)
+
+        def net_if_addrs():
+            nics = {}
+            nics["wlan0"] = [MagicMock(family=AF_LINK, address="00:00:00:00:00:00")]
+            nics["eth0"] = [MagicMock(family=AF_LINK, address="22:22:22:22:22:22")]
+            if with_ap:
+                nics["wlan_ap0"] = [
+                    MagicMock(family=AF_LINK, address="11:11:11:11:11:11")
+                ]
+            return nics
+
+        mocker.patch(
+            "pt_miniscreen.pages.network.mac_addresses.net_if_addrs",
+            net_if_addrs,
+        )
+
+    # AP interface not available, MAC address isn't displayed
+    mock_net_if_addrs(with_ap=False)
+
+    # scroll to mac page
+    miniscreen.down_button.release()
+    sleep(1)
+    miniscreen.down_button.release()
+    sleep(1)
+    miniscreen.down_button.release()
+    sleep(1)
+    miniscreen.down_button.release()
+    sleep(1)
+
+    snapshot.assert_match(miniscreen.device.display_image, "mac_addresses_no_ap.png")
+
+    # Display AP MAC address
+    mock_net_if_addrs(with_ap=True)
+    sleep(2)
+    snapshot.assert_match(miniscreen.device.display_image, "mac_addresses_with_ap.png")
