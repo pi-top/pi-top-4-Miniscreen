52d56b234a919a24c226dc5b77cc2747c76c6094
Jorge Capona <jcapona@gmail.com>
2024-07-11 17:52:52 -0400

    Add apt hook to create last-update breadcrumb

--- pt_miniscreen/pages/system/last_update.py
+++ pt_miniscreen/pages/system/last_update.py
@@ -24,7 +24,7 @@ def get_ip_url():
 
 
 def _get_last_update_breadcrumb_mtime():
-    filename = "/var/lib/pt-os-web-portal/last-update"
+    filename = "/var/lib/pt-miniscreen/last-update"
     try:
         # Find last modified date from breadcrumb
         mtime = Path(filename).stat().st_mtime
